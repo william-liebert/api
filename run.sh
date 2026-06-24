029a254e3ce1ff15e3a0175582fd1003025c21ae
WALiebert <William.Liebert@Outlook.com>
2026-06-23 22:43:18 -0700

    Guard deployments;

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-echo "Building and deploying to local Minikube instance..."
+if ! command -v kubectl &> /dev/null; then
+    echo "Error: kubectl is not installed or not in PATH."
+    exit 1
+fi
 
 if ! command -v minikube &> /dev/null; then
     echo "Error: Minikube is not installed or not in PATH."
@@ -10,31 +13,31 @@ if ! command -v minikube &> /dev/null; then
 fi
 
 if ! minikube status | grep -q "Running"; then
-    echo "Starting Minikube with Docker driver..."
+    echo "Starting Minikube..."
     minikube start --driver=docker
 fi
 
-if ! command -v kubectl &> /dev/null; then
-    echo "Error: kubectl is not installed or not in PATH."
-    exit 1
-fi
-
 if ! kubectl get namespace argocd &> /dev/null; then
+    echo "Creating ArgoCD namespace..."
     kubectl create namespace argocd
 fi
-    
-echo "Installing ArgoCD..."
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=300s
 
-echo "Installing Git server..."
-kubectl apply -n argocd -f kubernetes/git-server-deployment.yaml
-kubectl wait --for=condition=ready pod -l app=git-server -n argocd --timeout=300s
+if ! kubectl get deployment argocd-server -n argocd &> /dev/null; then
+    echo "Installing ArgoCD..."
+    kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+    kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=300s
+fi
+
+if ! kubectl get deployment git-server -n argocd &> /dev/null; then
+    echo "Installing Git Server..."
+    kubectl apply -n argocd -f kubernetes/git-server-deployment.yaml
+    kubectl wait --for=condition=ready pod -l app=git-server -n argocd --timeout=300s
+fi
 
 echo "Building Docker images..."
 docker build -t wtech-API:latest -f src/WTech.API/Dockerfile src/WTech.API
 
-echo "Deploying..."
+echo "Pushing Git Branch..."
 GIT_SERVER_IP=$(kubectl get service git-server-service -o jsonpath='{.spec.clusterIP}')
 GIT_REMOTE="git://$GIT_SERVER_IP:9418/git"
 if ! git remote get-url minikube-git &> /dev/null; then
