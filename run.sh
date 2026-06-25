a37b98d223a4ecf352b2e553e7fbaa3a4f871344
WALiebert <William.Liebert@Outlook.com>
2026-06-24 21:24:00 -0700

    Cleanup;

--- run.sh
+++ run.sh
@@ -22,13 +22,9 @@ helm upgrade --install gitea gitea-charts/gitea
 GIT_REMOTE="git://$MINIKUBE_IP:3000/wtech-api.git"
 echo "Git Remote: [$GIT_REMOTE]"
 
-echo "Installing Prometheus via Helm..."
+echo "Installing Prometheus Stack via Helm..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack
-
-echo "Installing Grafana via Helm..."
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm upgrade --install grafana prometheus-community/grafana
 
 echo "Building Docker images..."
 docker build -t wtech-api:latest -f src/WTech.API/Dockerfile .
