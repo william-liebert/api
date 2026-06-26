d18311ba538b520d35af69bc00451b21e8b4bd09
WALiebert <William.Liebert@Outlook.com>
2026-06-26 00:06:07 -0700

    nit;

--- run.sh
+++ run.sh
@@ -14,19 +14,19 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 echo "Installing ArgoCD via Helm..."
 helm repo add argo https://argoproj.github.io/argo-helm
-helm upgrade --install argo-cd argo/argo-cd
+helm upgrade --install argo-cd argo/argo-cd -f kubernetes/helm/argo-cd/values.yaml
 ARGOCD_ENDPOINT=$((minikube service argo-cd-argocd-server --url &) | grep -m 1 -o 'http://[^ ]*')
 ARGOCD_ADMIN_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
 echo "Installing Gitea via Helm with NodePort..."
 helm repo add gitea-charts https://dl.gitea.com/charts/
-helm upgrade --install gitea gitea-charts/gitea
+helm upgrade --install gitea gitea-charts/gitea -f kubernetes/helm/gitea/values.yaml
 GITEA_ENDPOINT=$((minikube service gitea-http --url &) | grep -m 1 -o 'http://[^ ]*')
 
 echo "Installing Prometheus Stack via Helm..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack
-GRAFANA_ENDPOINT=$((minikube service prometheus-nodeport --url &) | grep -m 1 -o 'http://[^ ]*')
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f kubernetes/helm/prometheus/values.yaml
+GRAFANA_ENDPOINT=$((minikube service prometheus-kube-prometheus-prometheus --url &) | grep -m 1 -o 'http://[^ ]*')
 
 echo "Building Docker images..."
 for dockerfile in src/*/Dockerfile ; do
@@ -35,22 +35,16 @@ for dockerfile in src/*/Dockerfile ; do
 done
 
 echo "Pushing Git Branch..."
-MINIKUBE_IP=$(minikube ip)
-MINIKUBE_GIT_URL="http://$MINIKUBE_IP:$GITEA_PORT/wtech-api.git"
-git remote add minikube-git "$MINIKUBE_GIT_URL" || git remote set-url minikube-git "$MINIKUBE_GIT_URL"
+git remote add minikube-git "$GITEA_ENDPOINT/git/" || git remote set-url minikube-git "$GITEA_ENDPOINT/git/"
 git push minikube-git --all -vvv
 
 echo "Syncing Deployments..."
 argocd app sync wtech-api || true
 
-echo "Gitea is now accessible via NodePort:"
-echo "  HTTP: $MINIKUBE_GIT_URL"
-echo "  SSH: ssh://git@$(minikube ip):30022"
-
 echo "Done."
 echo "ArgoCD Credentials: [Username: \"admin\", Password: \"$ARGOCD_ADMIN_PASSWORD\"]"
 echo "ArgoCD endpoint: [$ARGOCD_ENDPOINT]"
-echo "Gitea endpoint: [$MINIKUBE_GIT_URL]"
+echo "Gitea endpoint: [$GITEA_ENDPOINT]"
 echo "Grafana endpoint: [$GRAFANA_ENDPOINT]"
 
 # wait forever to keep the script running
