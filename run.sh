1b88892b4f17c12c2e5754cfdd7ac1ae86e5e950
WALiebert <William.Liebert@Outlook.com>
2026-06-25 21:32:35 -0700

    Fix

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 if ! minikube status > /dev/null 2>&1; then
-    minikube start --driver=docker
+    minikube start --driver=docker &
     while ! minikube status > /dev/null 2>&1; do
         sleep 1
     done
