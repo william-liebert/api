70cab4560b98994c479be8be27c035965f1ca39e
WALiebert <William.Liebert@Outlook.com>
2026-06-26 20:17:18 -0700

    WIP;

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-# Disabling for now, fix later. Don't worry about it for now.
-# if minikube status > /dev/null 2>&1; then
-#     minikube start --driver=docker &
-#     while ! minikube status > /dev/null 2>&1; do
-#         sleep 15
-#     done
-# fi
+if ! minikube status > /dev/null 2>&1; then
+    minikube start --driver=docker &
+    while ! minikube status > /dev/null 2>&1; do
+        sleep 15
+    done
+fi
+MINIKUBE_IP=$(minikube ip)
 
 echo "Installing Helm..."
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
@@ -16,18 +16,24 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 echo "Installing ArgoCD via Helm..."
 helm repo add argo https://argoproj.github.io/argo-helm
 helm upgrade --install argo-cd argo/argo-cd -f kubernetes/helm/argo-cd/values.yaml
-ARGOCD_ENDPOINT=$((minikube service argo-cd-argocd-server --url &) | grep -m 1 -o 'http://[^ ]*')
+ARGOCD_PORT=30080
+kubectl port-forward svc/argo-cd-argocd-server $ARGOCD_PORT:80 &
+ARGOCD_ENDPOINT="http://$MINIKUBE_IP:$ARGOCD_PORT"
 ARGOCD_ADMIN_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
 echo "Installing Gitea via Helm with NodePort..."
 helm repo add gitea-charts https://dl.gitea.com/charts/
 helm upgrade --install gitea gitea-charts/gitea -f kubernetes/helm/gitea/values.yaml
-GITEA_ENDPOINT=$((minikube service gitea-http --url &) | grep -m 1 -o 'http://[^ ]*')
+GITEA_PORT=3000
+kubectl port-forward svc/gitea-http $GITEA_PORT:3000 &
+GITEA_ENDPOINT="http://$MINIKUBE_IP:$GITEA_PORT"
 
 echo "Installing Prometheus Stack via Helm..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f kubernetes/helm/prometheus/values.yaml
-GRAFANA_ENDPOINT=$((minikube service prometheus-kube-prometheus-prometheus --url &) | grep -m 1 -o 'http://[^ ]*')
+GRAFANA_PORT=9090
+kubectl port-forward svc/prometheus-kube-prometheus-stack-grafana $GRAFANA_PORT:9090 &
+GRAFANA_ENDPOINT="http://$MINIKUBE_IP:$GRAFANA_PORT"
 
 echo "Building Docker images..."
 for dockerfile in src/*/Dockerfile ; do
@@ -36,7 +42,7 @@ for dockerfile in src/*/Dockerfile ; do
 done
 
 echo "Pushing Git Branch..."
-git remote add minikube-git "$GITEA_ENDPOINT/git/" || git remote set-url minikube-git "$GITEA_ENDPOINT/git/"
+git remote add minikube-git "$GITEA_ENDPOINT/admin/wtech-api.git" || git remote set-url minikube-git "$GITEA_ENDPOINT/admin/wtech-api.git"
 git push minikube-git --all -vvv
 
 echo "Syncing Deployments..."
