270956cdee2c9b0a14f11c7fae4aa7bd7a2bb619
WALiebert <William.Liebert@Outlook.com>
2026-06-26 21:40:24 -0700

    nit;

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# trap 'kill $(jobs -p)' EXIT INT TERM
+trap 'kill $(jobs -p)' EXIT INT TERM
 
 if ! minikube status > /dev/null 2>&1; then
     minikube start --driver=docker &
@@ -10,7 +10,6 @@ if ! minikube status > /dev/null 2>&1; then
         sleep 15
     done
 fi
-MINIKUBE_IP=$(minikube ip)
 
 echo "Installing Helm..."
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
@@ -20,7 +19,7 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm upgrade --install argo-cd argo/argo-cd -f kubernetes/helm/argo-cd/values.yaml
 ARGOCD_PORT=30080
 kubectl port-forward svc/argo-cd-argocd-server $ARGOCD_PORT:80 &
-ARGOCD_ENDPOINT="http://$MINIKUBE_IP:$ARGOCD_PORT"
+ARGOCD_ENDPOINT="http://127.0.0.1:$ARGOCD_PORT"
 ARGOCD_ADMIN_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
 echo "Installing Gitea via Helm with NodePort..."
@@ -28,15 +27,15 @@ helm repo add gitea-charts https://dl.gitea.com/charts/
 helm upgrade --install gitea gitea-charts/gitea -f kubernetes/helm/gitea/values.yaml
 GITEA_PORT=3000
 kubectl port-forward svc/gitea-http $GITEA_PORT:3000 &
-kubectl port-forward svc/gitea-ssh $GITEA_PORT:22 &
-GITEA_ENDPOINT="git@$MINIKUBE_IP:$GITEA_PORT:admin/wtech-api.git"
+GITEA_ENDPOINT="http://127.0.0.1:$GITEA_PORT"
+GIT_REPO_ENDPOINT="$GITEA_ENDPOINT/localadmin/wtech-api.git"
 
 echo "Installing Prometheus Stack via Helm..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f kubernetes/helm/prometheus/values.yaml
 GRAFANA_PORT=9090
 kubectl port-forward svc/prometheus-kube-prometheus-stack-grafana $GRAFANA_PORT:9090 &
-GRAFANA_ENDPOINT="http://$MINIKUBE_IP:$GRAFANA_PORT"
+GRAFANA_ENDPOINT="http://127.0.0.1:$GRAFANA_PORT"
 
 echo "Building Docker images..."
 for dockerfile in src/*/Dockerfile ; do
@@ -45,16 +44,19 @@ for dockerfile in src/*/Dockerfile ; do
 done
 
 echo "Pushing Git Branch..."
-git remote add minikube-git "$GITEA_ENDPOINT" || git remote set-url minikube-git "$GITEA_ENDPOINT"
-git push minikube-git --all -vvv
+git remote add minikube-git "$GIT_REPO_ENDPOINT" || git remote set-url minikube-git "$GIT_REPO_ENDPOINT"
+git push minikube-git --all
 
 echo "Syncing Deployments..."
 argocd app sync wtech-api || true
 
+set +x
 echo "Done."
+echo ""
 echo "ArgoCD Credentials: [Username: \"admin\", Password: \"$ARGOCD_ADMIN_PASSWORD\"]"
 echo "ArgoCD endpoint: [$ARGOCD_ENDPOINT]"
 echo "Gitea endpoint: [$GITEA_ENDPOINT]"
+echo "Git Repository endpoint: [$GIT_REPO_ENDPOINT]"
 echo "Grafana endpoint: [$GRAFANA_ENDPOINT]"
 
 wait
