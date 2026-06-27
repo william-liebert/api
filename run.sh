1b8f2250bef63190c516b830cb81c77774205327
WALiebert <William.Liebert@Outlook.com>
2026-06-27 15:03:34 -0700

    Cleanup;

--- run.sh
+++ run.sh
@@ -23,28 +23,24 @@ set -ex
 echo "Applying Terraform..."
 terraform -chdir=terraform/local/helm init
 terraform -chdir=terraform/local/helm apply -auto-approve
+kubectl port-forward svc/gitea-http 3000:3000 &
+GITEA_ENDPOINT="http://127.0.0.1:3000"
 terraform -chdir=terraform/local/gitea init
 terraform -chdir=terraform/local/gitea apply -auto-approve
+kubectl port-forward svc/argo-cd-argocd-server 30080:80 &
+ARGOCD_ENDPOINT="http://127.0.0.1:30080"
+ARGOCD_ADMIN_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 terraform -chdir=terraform/local/argocd init
 terraform -chdir=terraform/local/argocd apply -auto-approve
+kubectl port-forward svc/prometheus-kube-prometheus-stack-grafana 9090:9090 &
+GRAFANA_ENDPOINT="http://127.0.0.1:9090"
 
 echo "Pushing Git Branch..."
+GIT_REPO_ENDPOINT="$GITEA_ENDPOINT/localadmin/wtech-api.git"
 git remote add minikube-git "$GIT_REPO_ENDPOINT" || git remote set-url minikube-git "$GIT_REPO_ENDPOINT"
 git push minikube-git --all
 
 set +x
-
-ARGOCD_PORT=30080
-kubectl port-forward svc/argo-cd-argocd-server $ARGOCD_PORT:80 &
-ARGOCD_ENDPOINT="http://127.0.0.1:$ARGOCD_PORT"
-ARGOCD_ADMIN_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-GITEA_PORT=3000
-kubectl port-forward svc/gitea-http $GITEA_PORT:3000 &
-GITEA_ENDPOINT="http://127.0.0.1:$GITEA_PORT"
-GIT_REPO_ENDPOINT="$GITEA_ENDPOINT/localadmin/wtech-api.git"
-GRAFANA_PORT=9090
-kubectl port-forward svc/prometheus-kube-prometheus-stack-grafana $GRAFANA_PORT:9090 &
-GRAFANA_ENDPOINT="http://127.0.0.1:$GRAFANA_PORT"
 echo "Done."
 echo ""
 echo "ArgoCD Credentials: [Username: \"admin\", Password: \"$ARGOCD_ADMIN_PASSWORD\"]"
--- terraform/local/helm/main.tf
+++ terraform/local/helm/main.tf
@@ -2,8 +2,6 @@ resource "helm_release" "argocd" {
   name            = "argocd"
   repository      = "https://argoproj.github.io/argo-helm"
   chart           = "argo-cd"
-  cleanup_on_fail = true
-  take_ownership  = true
   version         = "10.0.0"
 
   values = [
@@ -15,8 +13,6 @@ resource "helm_release" "gitea" {
   name            = "gitea"
   repository      = "https://dl.gitea.com/charts/"
   chart           = "gitea"
-  cleanup_on_fail = true
-  take_ownership  = true
   version         = "12.6.0"
 
   values = [
@@ -28,8 +24,6 @@ resource "helm_release" "gitea" {
 #   name            = "prometheus"
 #   repository      = "https://prometheus-community.github.io/helm-charts"
 #   chart           = "prometheus"
-#   cleanup_on_fail = true
-#   take_ownership  = true
 #   version         = "15.0.1"
 
 #   values = [
